988cd8117eda936f80c56380d4e18c856b7e5610
PraDika24 <handukaagus108@gmail.com>
2024-06-22 22:16:44 +0800

    fix splash screen

--- app/src/main/java/com/example/uangin/MainActivity.kt
+++ app/src/main/java/com/example/uangin/MainActivity.kt
@@ -1,20 +1,21 @@
 package com.example.uangin
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import android.os.Handler
 
 class MainActivity : AppCompatActivity() {
+
+    private val SPLASH_TIME_OUT: Long = 1000 // Durasi splash screen dalam milidetik (3 detik)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.home)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.home)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        setContentView(R.layout.activity_main)
+        Handler().postDelayed({
+            startActivity(Intent(this, HomeActivity::class.java))
+            finish()
+        }, SPLASH_TIME_OUT)
     }
+
+
 }
