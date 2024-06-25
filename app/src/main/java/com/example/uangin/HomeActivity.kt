0e1e76405f2a716151e03567de7dff4cc17ad393
Kelvin Andhika <kelvinandhika11@gmail.com>
2024-06-25 22:45:28 +0800

    halaman home setengah jadi

--- app/src/main/java/com/example/uangin/HomeActivity.kt
+++ app/src/main/java/com/example/uangin/HomeActivity.kt
@@ -10,7 +10,7 @@ class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.home)
+        setContentView(R.layout.activity_home)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.home)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
