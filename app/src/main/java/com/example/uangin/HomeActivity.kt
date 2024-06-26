e434429957a815a709a9efaa08b8a14f68e32de8
Kelvin Andhika <kelvinandhika11@gmail.com>
2024-06-26 18:18:48 +0800

    catat keuangan dan home jadi

--- app/src/main/java/com/example/uangin/HomeActivity.kt
+++ app/src/main/java/com/example/uangin/HomeActivity.kt
@@ -3,14 +3,21 @@ package com.example.uangin
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_home)
-
+        val fab: FloatingActionButton = findViewById(R.id.fab)
+        fab.setOnClickListener {
+            val intent = Intent(this@HomeActivity, AddActivity::class.java)
+            startActivity(intent)
         }
     }
+}
