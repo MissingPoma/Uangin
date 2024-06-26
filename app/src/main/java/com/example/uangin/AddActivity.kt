9880e38f80fd59b479825993e334d4f1baf257d4
PraDika24 <handukaagus108@gmail.com>
2024-06-27 00:37:51 +0800

    nambahin kode di addactivity

--- app/src/main/java/com/example/uangin/AddActivity.kt
+++ app/src/main/java/com/example/uangin/AddActivity.kt
@@ -1,12 +1,19 @@
 package com.example.uangin
 
+import android.app.DatePickerDialog
 import android.content.Intent
 import android.os.Bundle
 import android.widget.ImageButton
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import java.text.SimpleDateFormat
+import java.util.*
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class AddActivity : AppCompatActivity() {
+
+    private lateinit var tanggalTextView: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_adding_data)
@@ -16,5 +23,40 @@ class AddActivity : AppCompatActivity() {
             val intent = Intent(this, HomeActivity::class.java)
             startActivity(intent)
         }
+
+        tanggalTextView = findViewById(R.id.tanggalTextView)
+        val calendarButton: ImageButton = findViewById(R.id.calendarButton)
+
+        calendarButton.setOnClickListener {
+            showDatePickerDialog()
+        }
     }
+
+    private fun showDatePickerDialog() {
+        val calendar = Calendar.getInstance()
+        val year = calendar.get(Calendar.YEAR)
+        val month = calendar.get(Calendar.MONTH)
+        val dayOfMonth = calendar.get(Calendar.DAY_OF_MONTH)
+
+        val datePickerDialog = DatePickerDialog(
+            this,
+            { _, year1, monthOfYear, dayOfMonth1 ->
+                val selectedDate = "$dayOfMonth1 ${getMonthName(monthOfYear)} $year1"
+                tanggalTextView.text = selectedDate
+            },
+            year, month, dayOfMonth
+        )
+
+        datePickerDialog.show()
+    }
+
+    private fun getMonthName(month: Int): String {
+        val monthNames = arrayOf(
+            "Januari", "Februari", "Maret", "April", "Mei", "Juni",
+            "Juli", "Agustus", "September", "Oktober", "November", "Desember"
+        )
+        return monthNames[month]
+    }
+
+
 }
